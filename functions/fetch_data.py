f2053ffb413d7ba2353fa9f279534dcd7d74103b
Furkan Kucuk <furkankucuk@outlook.com>
2023-11-10 12:03:02 +0300

    Add tqdm progress bar to fetch_data function

--- functions/fetch_data.py
+++ functions/fetch_data.py
@@ -1,23 +1,30 @@
 import csv
 import requests
 import os
+from tqdm.auto import tqdm
+
+def total_rows(csv_file_path):
+    with open(csv_file_path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+    return sum(1 for _ in reader)
 
 def fetch_data(csv_file_path):
-
 # Open the CSV file
     with open(csv_file_path, 'r') as csvfile:
         
         reader = csv.DictReader(csvfile)
 
         # Loop through each row in the CSV file
-        for row in reader:
+        for row in tqdm(reader, desc='Downloading', total=total_rows(csv_file_path)):
             # Extract the assetId, rawLink, and filetype    
-            assetId = row['assetId']
+            downloadAttribute = row['downloadAttribute']
+            if '8K-PNG' != downloadAttribute:
+                continue
             rawLink = row['rawLink']
-            filetype = row['filetype']
 
             # Download the file
-            filename = f'{assetId}_{filetype}.{filetype}'
+            # Extract the filename from the rawLink
+            filename = rawLink.split('/')[-1]
             filepath = os.path.join('Data', filename)
 
             if not os.path.exists(filepath):
